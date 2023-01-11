d645b3c9df47ecd68ceabfb0b10fb21ec0590ba5
jmlayton <91229141+jmlayton@users.noreply.github.com>
2023-01-11 14:54:08 -0500

    Delete Dada2_Config.R

--- Janja/Dada2/Dada2_Config.R
+++
@@ -1,8 +0,0 @@
-f_primer_len <- 19
-r_primer_len <- 20
-maxEE <- 2
-maxN <- 0
-f_truncLen <- 151
-r_truncLen <- 148
-Janja_master_dir <- '/home/share/Jonathan/TEST/Code/Bioinformatics2/Bioinformatics'
-Demultiplexed_Seqs_Directory <-  '/home/share/Jonathan/TEST/Data/Sequence_Batches/Seq_Batch_1214_test/Demultiplexed_Seqs'
